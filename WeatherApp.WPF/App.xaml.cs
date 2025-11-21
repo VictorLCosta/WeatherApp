851d0bbd9a7bac02feb14caf4663ea29d2b00580
VictorLCosta <victorlc2019@outlook.com>
2025-11-21 11:37:08 -0300

    ✨ feat: implement generice host

--- WeatherApp.WPF/App.xaml.cs
+++ WeatherApp.WPF/App.xaml.cs
@@ -1,9 +1,38 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace WeatherApp.WPF;
 
 public partial class App : Application
 {
+    private readonly IHost _host;
+
+    public App()
+    {
+        _host = CreateHostBuilder().Build();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[]? args = null) =>
+        Host.CreateDefaultBuilder(args)
+            .AddViews();
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _host.Start();
+
+        Window window = _host.Services.GetRequiredService<MainWindow>();
+        window.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await _host.StopAsync();
+        _host.Dispose();
+
+        base.OnExit(e);
+    }
+
 }
---
+++ WeatherApp.WPF/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WeatherApp.WPF;
+
+internal static class Extensions
+{
+    internal static IHostBuilder AddViews(this IHostBuilder host)
+    {
+        host.ConfigureServices(services =>
+        {
+            services.AddSingleton(s => new MainWindow());
+        });
+
+        return host;
+    }
+}
