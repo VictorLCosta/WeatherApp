490be7baad5032877350496bc856601b24564958
VictorLCosta <victorlc2019@outlook.com>
2025-11-22 23:20:25 -0300

    ✨ feat: implements value converter for if its raining or not

---
+++ WeatherApp.WPF/ValueConverters/BoolToRainConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WeatherApp.WPF.ValueConverters;
+
+public class BoolToRainConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        bool isRaining = (bool)value;
+
+        if (isRaining)
+            return "Currently raining";
+        else
+            return "Not Raining at all";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        string isRaining = value.ToString()!;
+
+        if (isRaining == "Currently raining")
+            return true;
+
+        return false;
+    }
+}
